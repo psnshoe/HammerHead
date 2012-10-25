3a2f6acf496a44f47b97bd141b5cecb2195151ec
James Schumacher <jschumacher@DEV-JS-L01.psn.com>
2012-10-25 09:50:19 -0500

    commit changes to Form_AddPerson

---
+++ TestSolution1/FormsApp1/Form_AddPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormsApp1
+{
+    public partial class Form_AddPerson : Form
+    {
+        public Form_AddPerson()
+        {
+            InitializeComponent();
+        }
+    }
+}
